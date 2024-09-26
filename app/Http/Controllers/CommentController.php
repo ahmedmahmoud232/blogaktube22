4da046045eecd6dccdef324999d0715ffcc4a34f
ahmedmahmoud232 <ahmedmahmoud22990@gmail.com>
2024-09-26 08:38:52 +0300

    22

---
+++ app/Http/Controllers/CommentController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use App\Models\Post;
+use App\Models\Comment;
+
+class CommentController extends Controller
+{
+    //
+}
